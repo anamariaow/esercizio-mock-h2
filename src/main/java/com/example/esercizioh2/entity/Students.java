02f969e3373c9a69d44eec443e3f210cad33ce5c
anamariaow <new_annemarie@yahoo.com>
2024-03-06 02:42:37 +0100

    Esercizio mock con h2 con username: testdb e password: psw1234 all'indirizzo     //localhost:8080/h2-console

---
+++ src/main/java/com/example/esercizioh2/entity/Students.java
@@ -0,0 +1,61 @@
+package com.example.esercizioh2.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Students {
+    //localhost:8080/h2-console
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    public Students(Long id, String lastName, String firstName, String email) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public Students() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/test/java/com/example/esercizioh2/EsercizioH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esercizioh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
